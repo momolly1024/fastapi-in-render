6535651cadd6db7c432c7ec05b54d39ea016bb68
MollyCognito <momolly1024@gmail.com>
2023-02-09 15:02:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# FastAPI in Render
+
+uvicorn main:app --reload
+
+### Start command
+
+### file setting
+
+### env setting
---
+++ cors.py
@@ -0,0 +1,42 @@
+from typing import Union
+
+from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+key = os.getenv("FAKE_VALUE")
+print(key)
+
+app = FastAPI()
+
+ 
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:8080",
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+
+@app.get("/")
+async def main():
+    return {"message": key}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
